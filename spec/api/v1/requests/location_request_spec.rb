06083f310b34c7d328d8be627b5bf93920346655
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-11-07 16:04:54 -0700

    GraphQL query and mutation setip

--- spec/api/v1/requests/location_request_spec.rb
+++ spec/api/v1/requests/location_request_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Location Request API' do
   describe 'Location Request/Response path' do
     it 'can get a location' do
-      params = { zip: 80226 }
-      GET "/api/v1/location", params: params
+      # params = { zip: 80226 }
+      post "/graphql"
       require 'pry'; binding.pry
     end
   end
