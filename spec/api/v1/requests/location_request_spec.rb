dc2757f80692e1b5cb4dbbb96e67a08546e71968
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-11-07 16:32:38 -0700

    graphql updates request test setup graphql_path

--- spec/api/v1/requests/location_request_spec.rb
+++ spec/api/v1/requests/location_request_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Location Request API' do
   describe 'Location Request/Response path' do
     it 'can get a location' do
-      # params = { zip: 80226 }
-      post "/graphql"
+      post graphql_path, params: { query: "80226" }
       require 'pry'; binding.pry
     end
   end
