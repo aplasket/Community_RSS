4b45b663e4965f58c6835c2a663859376c93d17f
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-11-10 11:42:29 -0600

    Add Community and query  type

---
+++ app/graphql/types/community_type.rb
@@ -0,0 +1,11 @@
+module Types
+  class CommunityType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :county, String, null: false
+    field :state, String, null: false
+    field :zip_code, Integer, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
