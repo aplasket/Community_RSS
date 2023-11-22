1d94a68d5bbd0d0ebee596f73d94ad1f2f3afd98
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-11-22 11:56:54 -0700

    Setup initial graphql query tests

--- spec/api/v1/requests/location_request_spec.rb
+++ spec/api/v1/requests/location_request_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Location Request API' do
-  describe 'Location Request/Response path' do
-    it 'can get a location' do
-      post graphql_path, params: { query: "80226" }
-      require 'pry'; binding.pry
+  describe 'mutation request' do
+    it 'get successful response' do
+      
+    end
+
+    it 'returns data for zip code in query' do
+
     end
   end
 end
