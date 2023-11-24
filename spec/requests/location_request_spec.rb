9cd9910b0c96305a663b4fb084d2e1f24571e142
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-11-24 15:37:17 -0700

    move zip code query test file location

--- spec/requests/location_request_spec.rb
+++ spec/requests/location_request_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.describe 'Location Request API' do
   describe 'mutation request' do
     it 'get successful response' do
-      query = 
-
+      query = <<-GQL
+        mutation {
+          zip_code(
+            zip_code: "80226"
+          )
+        }
+      GQL
 
       result = CommunityRssSchema.execute(query)
       expect(result.status).to be successful
