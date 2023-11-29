4c271e9d6a5e09a442007b5071494236f602aa5e
Ashley Plasket <ashley.plasket@gmail.com>
2023-11-28 19:36:19 -0500

    update arguments and resolve method

--- app/graphql/mutations/find_community.rb
+++ app/graphql/mutations/find_community.rb
@@ -1,23 +1,32 @@
 class Mutations::FindCommunity < Mutations::BaseMutation
-  null true
-  argument :zip_code, String
+  # null true
+  argument :zipCode, String, required: true
 
-  field :find_community, Types::CommunityType
+  # field :find_community, Types::CommunityType #can delete this out
+  field :message, String, null: false
   field :errors, [String], null: false
 
-  def resolve(body:, post_id:)
-    post = Post.find(post_id)
-    comment = post.comments.build(body: body, author: context[:current_user])
-    if comment.save
-      {
-        comment: comment,
-        errors: [],
-      }
-    else
-      {
-        comment: nil,
-        errors: comment.errors.full_messages
-      }
-    end
+
+  def resolve(zipCode:)
+    # fetch community info from service using zip code
+      # if fetch is successful
+        # {message: #whatever data you want to send to the frontend }
   end
+
+  
+  # def resolve(body:, post_id:)
+  #   post = Post.find(post_id)
+  #   comment = post.comments.build(body: body, author: context[:current_user])
+  #   if comment.save
+  #     {
+  #       comment: comment,
+  #       errors: [],
+  #     }
+  #   else
+  #     {
+  #       comment: nil,
+  #       errors: comment.errors.full_messages
+  #     }
+  #   end
+  # end
 end
